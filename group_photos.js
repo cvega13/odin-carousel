dfd76112727468adbe5520b551bab5a014e3ae09
Carlos Vega <carlosevega03@gmail.com>
2025-08-26 00:09:43 -0400

    PACT 2025

---
+++ group_photos.js
@@ -0,0 +1,22 @@
+const group_photos = [
+    [
+        "SNES",
+        [
+            "Donkey Kong Country (USA).png",
+            "EarthBound (USA).png",
+            "Legend of Zelda, The - A Link to the Past (USA).png",
+            "Super Mario Kart (USA).png",
+            "Super Mario RPG - Legend of the Seven Stars (USA).png",
+            "Super Mario World (USA).png",
+            "Super Mario World 2 - Yoshi's Island (USA).png",
+            "Super Metroid (USA).png"
+        ]
+    ]//,
+    // [
+    //      "Switch",
+    //]
+
+]
+
+
+module.exports = group_photos;
